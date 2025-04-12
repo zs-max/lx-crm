28264b3ee31feaacf209b1f795d9f3d7934b9fc1
lwk <lk_demo@163.com>
2025-04-12 16:09:47 +0800

    新增订单模块以及数据库连接和定义user实体

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GoodsModule } from './goods/goods.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [UserModule, GoodsModule],
+  imports: [
+    UserModule, 
+    GoodsModule,
+    TypeOrmModule.forRoot({
+      type: "mysql", //数据库类型
+      username: "root", //账号
+      password: "123456", //密码
+      host: "localhost", //host
+      port: 3306, //
+      database: "crmdb", //库名
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], //实体文件
+      synchronize:true, //synchronize字段代表是否自动将实体类同步到数据库
+      retryDelay:500, //重试连接数据库间隔
+      retryAttempts:10,//重试连接数据库的次数
+      autoLoadEntities:true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
+    }),
+    OrderModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
